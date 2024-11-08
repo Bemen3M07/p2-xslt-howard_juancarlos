1651d4aa4c38d8707c9f12b36cec3f29c38dbdc5
kkmanqlo <jomech23@bemen3.cat>
2024-11-08 20:41:20 +0100

    Borrado de archivo innecesario

--- Scripts/script_desplegableform.js
+++
@@ -1,8 +0,0 @@
-function toggleForm() {
-    const formulario = document.getElementById("formulario-videojuego");
-    if (formulario.style.display === "none") {
-        formulario.style.display = "block";
-    } else {
-        formulario.style.display = "none";
-    }
-}
